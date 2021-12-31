509ca940d2664ad3ca80da3d15717d40acd35c5f
difaagh <dhifa.aghisni@gmail.com>
2021-12-31 13:13:23 +0700

    update user_service test

--- src/services/user_service.test.ts
+++ src/services/user_service.test.ts
@@ -1,37 +1,78 @@
 import { Connection } from "typeorm";
+import { NewTestDB } from "@config/type_orm";
+import { Config } from "@config/config";
 import { UserService } from "./user_service";
-import { Config, NewTestDB } from "@config/index";
 import { User } from "@model/index";
-import { initializeTransactionalContext, patchTypeORMRepositoryWithBaseRepository } from "typeorm-transactional-cls-hooked";
-import { runInTransaction } from "typeorm-test-transactions";
+import { TransactionalTestContext } from "typeorm-transactional-tests";
 
-initializeTransactionalContext();
-patchTypeORMRepositoryWithBaseRepository();
+jest.mock("typeorm-transactional-cls-hooked", () => ({
+  Transactional: () => () => ({}),
+  BaseRepository: class {},
+}));
 
-let db: Connection;
+let testDB: Connection;
+let context: TransactionalTestContext;
 let userService: UserService;
 
 beforeAll(async () => {
+  testDB = await NewTestDB(new Config());
+  context = new TransactionalTestContext(testDB);
+  await context.start();
+  userService = new UserService(testDB);
+});
+
+afterAll(async () => {
+  if (testDB && context) {
+    await context.finish();
+    await testDB.close();
+  }
+});
+
+test("create-user", async () => {
+  const user: User = { email: "1@email.com", name: "john doe", password: "password" };
+  const createdUser = await userService.Create(user);
+
+  expect(createdUser).not.toEqual(undefined);
+  expect(createdUser.password).not.toEqual(user.password);
+});
+
+test("update-user", async () => {
+  const user: User = { email: "2@email.com", name: "john doe", password: "password" };
+  const createdUser = await userService.Create(user);
+
+  const userUpdate: User = { id: createdUser.id, email: "email@change.com", name: "doe john", password: "password2" };
+  const updatedUser = await userService.Update(userUpdate);
+
+  const isUserUpdated =
+    updatedUser.email !== createdUser.email && updatedUser.name !== createdUser.name && updatedUser.password !== createdUser.password;
+  expect(isUserUpdated).toBeTruthy();
+});
+
+test("find-by-user", async () => {
+  const user: User = { email: "3@email.com", name: "john doe", password: "password" };
+  const createdUser = await userService.Create(user);
+
+  const retriviedUser = await userService.FindBy("id", createdUser.id);
+
+  expect(retriviedUser).not.toEqual(undefined);
+});
+
+test("delete-user", async () => {
+  const user: User = { email: "4@email.com", name: "john doe", password: "password" };
+  const createdUser = await userService.Create(user);
+
   try {
-    const config = new Config();
-    db = await NewTestDB(config);
-    userService = new UserService(db);
+    await userService.Delete(createdUser.id);
   } catch (e) {
     throw Error(e);
   }
 });
 
-afterAll(async function () {
-  await db.close();
+test("login-user", async () => {
+  const user: User = { email: "5@email.com", name: "john doe", password: "password" };
+  await userService.Create(user);
+
+  const token = await userService.Login(user.email, user.password);
+
+  expect(token).toBeTruthy();
 });
-
-test(
-  "create user",
-  runInTransaction(async () => {
-    const user: User = { email: "hallo@email.com", name: "john doe", password: "password" };
-    const createdUser = await userService.Create(user);
-
-    expect(createdUser).not.toEqual(undefined);
-    expect(createdUser.password).not.toContain(user.password);
-  })
-);
