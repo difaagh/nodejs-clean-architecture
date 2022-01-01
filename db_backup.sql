1aa960ff0b6f47542d1ba3c05604aa1037409b57
difaagh <dhifa.aghisni@gmail.com>
2022-01-01 18:51:50 +0700

    delete sql, use migrations

--- db_backup.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `user_table` (
-  `id` varchar(36) NOT NULL,
-  `name` varchar(250) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(250) NOT NULL,
-`created_date` datetime,
-`updated_date` datetime,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
